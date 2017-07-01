2e31338984f593b631427c591bd091d4a2b7bb2d
Milan V. Kecojević <staog@gmx.com>
2017-07-01 22:57:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var counter = 5;
+var counter = 6;
 var position = 0;
 var positionButton = document.getElementsByClassName('position');
 var text = 'This is our first team project.';
 var startButton = document.getElementById('start');
 var writeText = document.getElementById('text');
 var userTextArea = document.getElementsByTagName('textarea')[0];
-var player1TextArea = document.getElementsByTagName('textarea')[1];
-var player2TextArea = document.getElementsByTagName('textarea')[2];
-var player3TextArea = document.getElementsByTagName('textarea')[3];
+var bot1TextArea = document.getElementsByTagName('textarea')[1];
+var bot2TextArea = document.getElementsByTagName('textarea')[2];
+var bot3TextArea = document.getElementsByTagName('textarea')[3];
 var userBtn = document.getElementById('user');
 var bot1btn = document.getElementById('bot1btn');
 var bot2btn = document.getElementById('bot2btn');
@@ -27,62 +27,62 @@ function countDown() {
     writeText.className = 'col-lg-6 col-lg-offset-3 text-center well';
     writeText.innerHTML = text;
     userTextArea.focus();
-    player1();
-    player2();
-    player3();
+    bot1();
+    bot2();
+    bot3();
     userTyping();
   }
 }
 
-function player1() {
+function bot1() {
   var textArr = text.split('');
   var newText = '';
 
   function start() {
     if (textArr.length !== 0) {
       newText += textArr.shift();
-      player1TextArea.innerHTML = newText;
+      bot1TextArea.innerHTML = newText;
       setTimeout(start, 150);
-    }else {
+    } else {
       position++;
-      ply1btn.innerHTML = "Position: " + position;
-      ply1btn.className = "btn btn-warning form-control";
+      bot1btn.innerHTML = "Position: " + position;
+      bot1btn.className = "btn btn-warning form-control";
     }
   }
   start();
 }
 
-function player2() {
+function bot2() {
   var textArr = text.split('');
   var newText = '';
 
   function start() {
     if (textArr.length !== 0) {
       newText += textArr.shift();
-      player2TextArea.innerHTML = newText;
+      bot2TextArea.innerHTML = newText;
       setTimeout(start, 200);
-    }else {
+    } else {
       position++;
-      ply2btn.innerHTML = "Position: " + position;
-      ply2btn.className = "btn btn-warning form-control";
+      bot2btn.innerHTML = "Position: " + position;
+      bot2btn.className = "btn btn-warning form-control";
     }
   }
   start();
 }
 
-function player3() {
+function bot3() {
   var textArr = text.split('');
   var newText = '';
 
   function start() {
     if (textArr.length !== 0) {
       newText += textArr.shift();
-      player3TextArea.innerHTML = newText;
+      bot3TextArea.innerHTML = newText;
       setTimeout(start, 250);
-    }else {
+    } else {
       position++;
-      ply3btn.innerHTML = "Position: " + position;
-      ply3btn.className = "btn btn-warning form-control";
+      bot3btn.innerHTML = "Position: " + position;
+      bot3btn.className = "btn btn-warning form-control";
     }
   }
   start();
@@ -99,7 +99,7 @@ function userFinished(e) {
     position++;
     user.innerHTML = "Position: " + position;
     user.className = "btn btn-warning form-control";
-  }else {
+  } else {
     user.innerHTML = "Wrong typing!";
     user.className = "btn btn-danger form-control";
   }
